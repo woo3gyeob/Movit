ef131b6458f4d787b138ac3a8a82ea438a7b436c
jongs3030 <jongs3030@naver.com>
2021-05-28 01:59:21 +0900

    last push

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from movies.models import Movie
 
 class User(AbstractUser):
     pass
--- server/community/admin.py
+++ server/community/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import Review, Comment
 
 # Register your models here.
+admin.site.register(Review)
+admin.site.register(Comment)
